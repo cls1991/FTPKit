d1796cf54b2b8650a54b34a1a94715c2bedf7452
cls1991 <1553556149@qq.com>
2015-07-07 00:04:27 +0800

    添加文件选择界面

---
+++ FTPKit/FTPKit/FTPFileChooserViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTPFileChooserViewController.h
+//  FTPKit
+//
+//  Created by 陶正凯 on 15/7/6.
+//  Copyright (c) 2015年 陶正凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTPFileChooserViewController : UIViewController
+
+@end
---
+++ FTPKit/FTPKit/FTPFileChooserViewController.m
@@ -0,0 +1,37 @@
+//
+//  FTPFileChooserViewController.m
+//  FTPKit
+//
+//  Created by 陶正凯 on 15/7/6.
+//  Copyright (c) 2015年 陶正凯. All rights reserved.
+//
+
+#import "FTPFileChooserViewController.h"
+
+@interface FTPFileChooserViewController ()
+
+@end
+
+@implementation FTPFileChooserViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
